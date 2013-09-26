24fa24563ff9624d6c6e05e654be300b30e4b03e
Chris Ollenburg <chris.ollenburg@gmail.com>
2013-09-25 21:38:54 -0400

    Added lights and an Int representation of the Binary number the user has turned on with the toggle switches

--- src/com/cardinal/cardinalhack/MainActivity.java
+++ src/com/cardinal/cardinalhack/MainActivity.java
@@ -13,85 +13,90 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity implements ListView.OnItemClickListener {
+public class MainActivity extends ActionBarActivity implements
+		ListView.OnItemClickListener {
 	private String[] mNavItems;
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
 	private ActionBarDrawerToggle mDrawerToggle;
 	private FragmentManager mFM;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        // TODO replace menu icon
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_closed)
-        {
-        	public void onDrawerClosed(View view) {
-        		
-        	}
-        	
-        	public void onDrawerOpened(View drawerView) {
-        		
-        	}
-        };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        
-        
-        mNavItems = getResources().getStringArray(R.array.nav_items);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.nav_row, mNavItems));
-        mDrawerList.setOnItemClickListener(this);
-        
-        mFM = getSupportFragmentManager(); 
-        FragmentTransaction ft = mFM.beginTransaction();
-        MainFragment fragment = new MainFragment();
-        ft.add(R.id.content_frame, fragment);
-        ft.commit();
-    }
-    
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
 
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		if (position == 0) {
-	        FragmentTransaction ft = mFM.beginTransaction();
-	        MainFragment fragment = new MainFragment();
-	        ft.add(R.id.content_frame, fragment);
-	        ft.commit();
-		} else if (position == 1) {
-	        FragmentTransaction ft = mFM.beginTransaction();
-	        MapFragment fragment = new MapFragment();
-	        ft.add(R.id.content_frame, fragment);
-	        ft.commit();
-		}
-        mDrawerLayout.closeDrawers();
-	}
-	
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-          return true;
-        }
-        // Handle your other action bar items...
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// TODO remove side menu
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_closed) {
+			public void onDrawerClosed(View view) {
 
-        return super.onOptionsItemSelected(item);
-    }
+			}
+
+			public void onDrawerOpened(View drawerView) {
+
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+
+		mNavItems = getResources().getStringArray(R.array.nav_items);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.nav_row,
+				mNavItems));
+		mDrawerList.setOnItemClickListener(this);
+
+		if (savedInstanceState == null) {
+			mFM = getSupportFragmentManager();
+			FragmentTransaction ft = mFM.beginTransaction();
+			MainFragment fragment = new MainFragment();
+			ft.add(R.id.content_frame, fragment);
+			ft.commit();
+		}
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		if (position == 0) {
+			FragmentTransaction ft = mFM.beginTransaction();
+			MainFragment fragment = new MainFragment();
+			ft.add(R.id.content_frame, fragment);
+			ft.commit();
+		} else if (position == 1) {
+			FragmentTransaction ft = mFM.beginTransaction();
+			MapFragment fragment = new MapFragment();
+			ft.add(R.id.content_frame, fragment);
+			ft.commit();
+		}
+		mDrawerLayout.closeDrawers();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle your other action bar items...
+
+		return super.onOptionsItemSelected(item);
+	}
 }
--- src/com/cardinal/cardinalhack/MainFragment.java
+++ src/com/cardinal/cardinalhack/MainFragment.java
@@ -1,43 +1,143 @@
 package com.cardinal.cardinalhack;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+/**
+ * Class implements a set of switches that represent binary numbers places.
+ * "Turning on" the right number will unlock the a new area of the app.
+ * 
+ * @author ChrisOllenburg
+ * 
+ */
 public class MainFragment extends Fragment implements OnClickListener {
-	private Button toggle1;
-	private Button toggle2;
-	private Button toggle3;
-	private Button toggle4;
-	
+	private static final String TAG = MainFragment.class.getSimpleName();
+	private static final String KEY_CURRENT_VALUE = "KEY_CURRENT_VALUE";
+	private Button switchFor8;
+	private Button switchFor4;
+	private Button switchFor2;
+	private Button switchFor1;
+	private Button lightFor8;
+	private Button lightFor4;
+	private Button lightFor2;
+	private Button lightFor1;
+	private Integer currentValue; // What the representation of the binary value is
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.main_fragment, container, false);
-		toggle1 = (Button) rootView.findViewById(R.id.toggleButton1);
-		toggle1.setOnClickListener(this);
-		toggle2 = (Button) rootView.findViewById(R.id.toggleButton2);
-		toggle2.setOnClickListener(this);
-		toggle3 = (Button) rootView.findViewById(R.id.toggleButton3);
-		toggle3.setOnClickListener(this);
-		toggle4 = (Button) rootView.findViewById(R.id.toggleButton4);
-		toggle4.setOnClickListener(this);
+		switchFor8 = (Button) rootView.findViewById(R.id.place8Switch);
+		switchFor8.setOnClickListener(this);
+		switchFor4 = (Button) rootView.findViewById(R.id.place4Switch);
+		switchFor4.setOnClickListener(this);
+		switchFor2 = (Button) rootView.findViewById(R.id.place2Switch);
+		switchFor2.setOnClickListener(this);
+		switchFor1 = (Button) rootView.findViewById(R.id.place1Switch);
+		switchFor1.setOnClickListener(this);
+		lightFor8 = (Button) rootView.findViewById(R.id.place8Light);
+		lightFor4 = (Button) rootView.findViewById(R.id.place4Light);
+		lightFor2 = (Button) rootView.findViewById(R.id.place2Light);
+		lightFor1 = (Button) rootView.findViewById(R.id.place1Light);
+		
+		super.onCreate(savedInstanceState);
+		if (savedInstanceState != null) {
+			currentValue = Integer.valueOf(savedInstanceState.getInt(KEY_CURRENT_VALUE));
+			Log.d(TAG, "Restored current value = " + currentValue.toString());
+			restore();
+		} else {
+			currentValue = Integer.valueOf(0);
+		}
+		
 		return rootView;
 	}
 	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	public void onSaveInstanceState(Bundle outState) {
+		outState.putInt(KEY_CURRENT_VALUE, currentValue);
+		super.onSaveInstanceState(outState);
 	}
 
 	@Override
 	public void onClick(View v) {
 		Button button = (Button)v;
 		button.setSelected(!button.isSelected());
+		int id = v.getId();
+		switch (id) {
+		case R.id.place8Switch:
+			if (button.isSelected()) {
+				currentValue += 8;
+				lightFor8.setBackgroundColor(Color.RED);
+			} else {
+				currentValue -= 8;
+				lightFor8.setBackgroundColor(Color.WHITE);
+			}
+			break;
+		case R.id.place4Switch:
+			if (button.isSelected()) {
+				currentValue += 4;
+				lightFor4.setBackgroundColor(Color.RED);
+			} else {
+				currentValue -= 4;
+				lightFor4.setBackgroundColor(Color.WHITE);
+			}
+			break;
+		case R.id.place2Switch:
+			if (button.isSelected()) {
+				currentValue += 2;
+				lightFor2.setBackgroundColor(Color.RED);
+			} else {
+				currentValue -= 2;
+				lightFor2.setBackgroundColor(Color.WHITE);
+			}
+			break;
+		case R.id.place1Switch:
+			if (button.isSelected()) {
+				currentValue += 1;
+				lightFor1.setBackgroundColor(Color.RED);
+			} else {
+				currentValue -= 1;
+				lightFor1.setBackgroundColor(Color.WHITE);
+			}
+			break;
+		default:
+			//Shouldn't get here
+		}
+		Log.d(TAG, "Current value of lights is " + currentValue.toString());
 	}
 	
+	/**
+	 * This Method will restore the light and switch positions after a restore
+	 *  from a savedInstanceState bundle
+	 */
+	private void restore() {
+		Log.d(TAG, "restore()");
+		int value = currentValue.intValue();
+		
+		if ((value&8) > 0) {
+			switchFor8.setSelected(true);
+			lightFor8.setBackgroundColor(Color.RED);
+		}
+		
+		if ((value&4) > 0) {
+			switchFor4.setSelected(true);
+			lightFor4.setBackgroundColor(Color.RED);
+		}
+		
+		if ((value&2) > 0) {
+			switchFor2.setSelected(true);
+			lightFor2.setBackgroundColor(Color.RED);
+		}
+		
+		if ((value&1) > 0) {
+			switchFor1.setSelected(true);
+			lightFor1.setBackgroundColor(Color.RED);
+		}
+	}
 }
